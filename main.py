b59d98eda8ab489f289b8d5f85d2f27ca796d88c
Yusuf Raheem <yusufraheem500@gmail.com>
2026-05-10 10:47:03 +0100

    Model trainer, evaluation and mlflow completed.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import (DataIngestionConfig, DataValidationConfig,
+                                                  DataTransformationConfig,ModelTrainerConfig)
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
@@ -29,8 +31,14 @@ if __name__=='__main__':
        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
        data_transformation=DataTransformation(data_validation_artifacts,data_transformation_config)
        logging.info("Initiate the data transformation")
-       data_transformation_artifacts=data_transformation.initiate_data_transform()
+       data_transformation_artifact=data_transformation.initiate_data_transform()
        logging.info("Data Transformation Completed!")
-       print(data_transformation_artifacts)
+       print(data_transformation_artifact)
+       #Model Trainer
+       logging.info("Model Training Started")
+       model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+       model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,
+                                  data_transformation_artifact=data_transformation_artifact)
+       model_trainer_artifact=model_trainer.initiate_model_trainer()
    except Exception as e:
        raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -10,11 +10,10 @@ TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phisingData.csv"
-
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
-
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+SAVED_MODEL_DIR = os.path.join("saved_models")
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -48,4 +47,14 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "n_neighbors": 3,
     "weights": "uniform",
 }
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
+"""
+Model trainer related constants start with MODEL TRAINER VAR NAME
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_FILE_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float=0.05
